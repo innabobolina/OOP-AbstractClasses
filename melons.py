a547e869b84e87f3f108a49cb363136c86991abd
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-11 12:44:23 -0700

    OO Melons Part1

---
+++ melons.py
@@ -0,0 +1,47 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder():
+    """An abstract base class that other Melon Orders inherit from."""
+
+    def __init__(self, species, qty, order_type, tax):
+        """Initialize melon order attributes."""
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+    def get_total(self):
+        """Calculate price, including tax."""
+
+        base_price = 5
+        total = (1 + self.tax) * self.qty * base_price
+
+        return total   
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A melon order within the USA."""
+    
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "domestic", 0.08)
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        super().__init__(species, qty, "international", 0.17)
+        """Initialize melon order attributes."""
+        self.country_code = country_code
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
